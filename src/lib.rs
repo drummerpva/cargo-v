7741fe0a6a4a759f970a842e813bce8e2dc43c13
Douglas Poma <douglaspoma@yahoo.com>
2023-01-27 18:43:44 -0300

    feat: ensure who accept v prefix and ensure than new version grather than current

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,44 @@ pub fn update_version_by_label(cargo_toml_content: String, version: VersionLabel
 }
 
 pub fn update_version(cargo_toml_content: String, version: String) -> String {
+    let version = version.replace("v", "");
     let old_version = get_version(&cargo_toml_content);
+    verify_new_version_is_grather(&old_version, &version);
     cargo_toml_content.replace(&old_version, &version)
+}
+fn verify_new_version_is_grather(old_version: &str, new_version: &str) {
+    let (old_major, old_minor, old_patch) = get_version_as_tuple(old_version);
+    let (new_major, new_minor, new_patch) = get_version_as_tuple(new_version);
+    let old_major: usize = old_major.parse().unwrap();
+    let old_minor: usize = old_minor.parse().unwrap();
+    let old_patch: usize = old_patch.parse().unwrap();
+    let new_major: usize = new_major.parse().unwrap();
+    let new_minor: usize = new_minor.parse().unwrap();
+    let new_patch: usize = new_patch.parse().unwrap();
+    if old_major != new_major {
+        if new_major < old_major {
+            panic!("You can not set a version lower than the current version");
+        };
+        if new_minor != 0 || new_patch != 0 {
+            panic!("You can not set a version lower than the current version");
+        }
+        return;
+    }
+    if old_minor != new_minor {
+        if new_minor < old_minor {
+            panic!("You can not set a version lower than the current version");
+        }
+        if new_patch != 0 {
+            panic!("You can not set a version lower than the current version");
+        }
+        return;
+    }
+    if new_patch < old_patch {
+        panic!("You can not set a version lower than the current version");
+    }
+    if new_patch == old_patch {
+        panic!("You can not set a version lower than the current version");
+    }
 }
 
 fn get_version(cargo_toml_content: &str) -> String {
@@ -105,5 +141,38 @@ mod test {
             update_version_by_label(input, VersionLabel::Major),
             expected
         );
+    }
+
+    #[test]
+    #[should_panic(expected = "You can not set a version lower than the current version")]
+    fn should_panic_on_version_patch_passed_lower_than_current() {
+        let input = String::from("[package]\n name = \"cargo-v\"\n version = \"0.0.2\"\n edition = \"2021\"\n# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html\n[dependencies]\n");
+        update_version(input, "0.0.1".into());
+    }
+    #[test]
+    #[should_panic(expected = "You can not set a version lower than the current version")]
+    fn should_panic_on_version_minor_passed_lower_than_current() {
+        let input = String::from("[package]\n name = \"cargo-v\"\n version = \"0.2.2\"\n edition = \"2021\"\n# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html\n[dependencies]\n");
+        update_version(input, "0.1.0".into());
+    }
+    #[test]
+    #[should_panic(expected = "You can not set a version lower than the current version")]
+    fn should_panic_on_version_major_passed_lower_than_current() {
+        let input = String::from("[package]\n name = \"cargo-v\"\n version = \"2.0.2\"\n edition = \"2021\"\n# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html\n[dependencies]\n");
+        update_version(input, "1.0.0".into());
+    }
+    #[test]
+    #[should_panic(expected = "InvalidDigit")]
+    fn should_panic_on_version_passed_had_negative_number() {
+        let input = String::from("[package]\n name = \"cargo-v\"\n version = \"2.0.2\"\n edition = \"2021\"\n# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html\n[dependencies]\n");
+        update_version(input, "-2.1.0".into());
+    }
+
+    #[test]
+    fn should_acept_v_prefix() {
+        let input = String::from("[package]\n name = \"cargo-v\"\n version = \"0.0.1\"\n edition = \"2021\"\n# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html\n[dependencies]\n");
+        let expected = String::from("[package]\n name = \"cargo-v\"\n version = \"0.0.2\"\n edition = \"2021\"\n# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html\n[dependencies]\n");
+
+        assert_eq!(update_version(input, "v0.0.2".into()), expected);
     }
 }
